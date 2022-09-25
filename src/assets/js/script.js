3b20cc3cdb4ab750cf757b338ff215b90db14963
hotarudesign <hotarudesign1220@gmail.com>
2022-09-25 19:12:30 +0900

    page add

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,15 +1,5 @@
 "use strict";
 
-gsap.set(".js-header", {
-  opacity: 0,
-});
-gsap.to(".js-header", {
-  opacity: 1,
-  scrollTrigger: {
-    trigger: ".mv",
-    start: "bottom center",
-  },
-});
 gsap.set(".js-card", {
   opacity: 0,
   y: 100,
@@ -22,4 +12,35 @@ gsap.to(".js-card", {
     trigger: ".feature",
     start: "top top",
   },
+});
+gsap.set(".js-hero-ttl", {
+  opacity: 0,
+  x: 200,
+});
+gsap.to(".js-hero-ttl", {
+  opacity: 1,
+  x: 0,
+});
+gsap.set(".js-feature-item", {
+  opacity: 0,
+  x: 100,
+});
+gsap.to(".js-feature-item", {
+  opacity: 1,
+  x: 0,
+  stagger: 0.2,
+  ease: Power2.easeOut,
+  scrollTrigger: {
+    trigger: ".feature",
+    start: "top center",
+  },
+});
+// headerスクロール背景変更
+const scrollHeader = document.getElementById("js-header");
+window.addEventListener("scroll", function () {
+  if (this.pageYOffset > 400) {
+    scrollHeader.classList.add("active");
+  } else {
+    scrollHeader.classList.remove("active");
+  }
 });
